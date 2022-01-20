df47fe1218f249f6de48eb7a06d6eef77e6f2c09
Christoph Liebender <c.liebender@vision4quality.de>
2022-01-20 17:09:34 +0100

    basic housekeeping stuff

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tweetinvi;
+using Tweetinvi.AspNet;
+using Tweetinvi.Models;
+using twitterXcrypto.util;
+
+Plugins.Add<AspNetPlugin>();
+
+var client = new TwitterClient(consumerKey: "1HqxAIikTtTkIF2FT1rAu5paw",
+                           consumerSecret: "IQ8INgkQshPTvdZPc11tedNOMKjihMdT0V3vnu5WsqF5ZrFpI4",
+                           bearerToken: "AAAAAAAAAAAAAAAAAAAAAJxOYQEAAAAACSMLy7fNoyv8ybUIGpKN6tpghoA%3DHqbB1VUNkQNb9eH4Nfe6UDCzK5W7oW9UObF6ah7IiXcjGYwnT0");
+var requestHandler = client.AccountActivity.CreateRequestHandler();
+var server = new SimpleHttpServer(8042);
+server.OnRequest += async (sender, context) =>
+{
+    Log.Write($"Incoming Request from {context.Request.UserHostAddress}; Name: {context.Request.UserHostName}");
+    var webhookRequest = WebhookRequestFactory.Create(context);
+
+    if (await requestHandler.IsRequestManagedByTweetinviAsync(webhookRequest))
+    {
+        if (await requestHandler.TryRouteRequestAsync(webhookRequest))
+        {
+            Log.Write("Handled Twitter-request sucessfully!");
+        }
+        else
+        {
+            Log.Write("Failed handling Twitter-request. Exiting...", Log.LogLevel.FTL);
+            Environment.Exit(1);
+        }        
+    }
+    else
+    {
+        Log.Write("Request was not from twitter!", Log.LogLevel.WRN);
+        StreamWriter sw = new(context.Response.OutputStream);
+        await sw.WriteAsync("Get out!!");
+        await sw.FlushAsync();
+        context.Response.StatusCode = 401;
+        context.Response.Close();
+    }
+};
+
+server.Start();
+
+// important stuff
+IWebhook webhook = await client.AccountActivity.CreateAccountActivityWebhookAsync("foo", "bar");
+
+await server.WaitUntilDisposed();
+Environment.Exit(0);
+
+
+
---
+++ util/Log.cs
@@ -0,0 +1,109 @@
+﻿using System.Runtime.InteropServices;
+using Serilog;
+
+namespace twitterXcrypto.util
+{
+    internal static class Log
+    {
+        #region base field
+        private static readonly ILogger _logger;
+        #endregion
+
+        #region properties
+        internal static string? LogPath { get; }
+        #endregion
+
+        #region ctor
+        static Log()
+        {
+            DirectoryInfo? logDir = null;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                logDir = Directory.CreateDirectory(@"/var/log/twitterXcrypto/");
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                logDir = new(Environment.CurrentDirectory);
+            }
+            else
+            {
+                Console.WriteLine("hehe you are not using supported os");
+                Environment.Exit(1);
+            }
+
+            LogPath = Path.Combine(logDir.FullName, "twixcry.log");
+
+            _logger = new LoggerConfiguration().WriteTo.Console()
+                                               .WriteTo.File(path: LogPath,
+                                                             rollingInterval: RollingInterval.Infinite,
+                                                             outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+                                               .CreateLogger();
+        }
+        #endregion
+
+        #region enum loglevel
+        public enum LogLevel
+        {
+            VRB,
+            DBG,
+            INF,
+            WRN,
+            ERR,
+            FTL
+        }
+        #endregion
+
+        #region methods
+        public static void Write(string message, LogLevel level = LogLevel.INF)
+        {
+            switch (level)
+            {
+                case LogLevel.VRB:
+                    _logger.Verbose(message);
+                    break;
+                case LogLevel.DBG:
+                    _logger.Debug(message);
+                    break;
+                case LogLevel.INF:
+                    _logger.Information(message);
+                    break;
+                case LogLevel.WRN:
+                    _logger.Warning(message);
+                    break;
+                case LogLevel.ERR:
+                    _logger.Error(message);
+                    break;
+                case LogLevel.FTL:
+                    _logger.Fatal(message);
+                    break;
+            }
+        }
+
+        public static void Write(string message, Exception e, LogLevel level = LogLevel.ERR)
+        {
+            switch (level)
+            {
+                case LogLevel.VRB:
+                    _logger.Verbose(e, message);
+                    break;
+                case LogLevel.DBG:
+                    _logger.Debug(e, message);
+                    break;
+                case LogLevel.INF:
+                    _logger.Information(e, message);
+                    break;
+                case LogLevel.WRN:
+                    _logger.Warning(e, message);
+                    break;
+                case LogLevel.ERR:
+                    _logger.Error(e, message);
+                    break;
+                case LogLevel.FTL:
+                    _logger.Error(e, message);
+                    break;
+            }
+        }
+        #endregion
+    }
+}
