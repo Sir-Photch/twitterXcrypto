a4c2a3fc82b574794486f44ece43c8950209c464
Christoph Liebender <c.liebender@vision4quality.de>
2022-02-16 18:18:48 +0100

    adding ocr barebones

--- csharp/src/imaging/OCR.cs
+++ csharp/src/imaging/OCR.cs
@@ -1,14 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Emgu.CV.OCR;
+﻿using Emgu.CV.OCR;
 
 namespace twitterXcrypto.imaging
 {
-    internal static class OCR
+    internal class OCR : IDisposable
     {
-        // TODO
+        internal static OCR Instance => _instance ??= new();
+
+        internal static string? Whitelist { get; set; }
+
+        internal async Task<string> GetText(Image image) => await Task.Run(() =>
+        {
+            _ocr.SetImage(image.Mat);
+            return _ocr.Recognize() is 0 ? _ocr.GetUTF8Text() : string.Empty;
+        });
+
+        public void Dispose() => _ocr.Dispose();
+
+        #region private
+
+        private static OCR? _instance;
+        private readonly Tesseract _ocr;
+        private static readonly string DATA_PATH = Directory.CreateDirectory(Path.Combine(Environment.CurrentDirectory, nameof(OCR))).FullName;
+
+        private OCR()
+        {
+            if (string.IsNullOrEmpty(Whitelist))
+                throw new InvalidOperationException("No whitelist provided");
+
+            _ocr = new Tesseract(DATA_PATH, "eng", OcrEngineMode.TesseractLstmCombined, Whitelist);
+        }
+
+        #endregion
     }
 }
